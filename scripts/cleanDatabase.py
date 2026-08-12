632014d0558f03d4fce90a74a85c645a39dca4af
sebastien <dumur.seb@gmail.com>
2026-08-12 18:41:23 +0200

    Add new bases regex

--- scripts/cleanDatabase.py
+++ scripts/cleanDatabase.py
@@ -5,18 +5,68 @@ from config import DB_CONFIG
 
 # Compile specific regex patterns to extract IDs precisely for each base
 urlPatterns = {
-    'gnd': re.compile(r'nid(?:=|%3D)(\d+)'),
-    'isni': re.compile(r'ISN(?::|%3A)([0-9X]{16})'),
+    # Extracts the numeric ID following 'nid=' or 'gnd/'
+    'gnd': re.compile(r'(?:nid(?:=|%3D)|gnd/)(\d+)', re.IGNORECASE),
+    # Extracts the 16-character ID following 'ISN:' or 'isni.org/isni/'
+    'isni': re.compile(r'(?:ISN(?::|%3A)|isni\.org/isni/)([0-9X]{16})', re.IGNORECASE),
+    # Extracts the numeric ID following 'id='
     'cths': re.compile(r'id=(\d+)'),
+    # Extracts the numeric ID following 'data.bnf.fr/', ignoring the optional '/fr/'
     'data bnf': re.compile(r'data\.bnf\.fr/(?:fr/)?(\d+)'),
+    # Extracts the numeric ID following 'viaf.org/viaf/'
     'viaf': re.compile(r'viaf\.org/viaf/(\d+)'),
+    # Extracts the alphanumeric ID following 'notProdId='
     'an': re.compile(r'notProdId=([A-Z0-9_]+)', re.IGNORECASE),
+    # Extracts the alphanumeric ID following 'notProdId='
     'archives nationales': re.compile(r'notProdId=([A-Z0-9_]+)', re.IGNORECASE),
+    # Extracts the numeric ID following 'idref.fr/'
     'idref': re.compile(r'idref\.fr/(\d+)'),
+    # Extracts the Q-number following 'Item:'
     'biblissima': re.compile(r'Item:(Q\d+)', re.IGNORECASE),
+    # Extracts the alphanumeric ID following 'resource/' or 'authority_id='
     'bne': re.compile(r'(?:resource/|authority_id=)([A-Z0-9]+)', re.IGNORECASE),
+    # Extracts the numeric ID following 'TRM=gnd:'
     'bsz': re.compile(r'TRM=gnd(?:%3A|:)(\d+)', re.IGNORECASE),
-    'cinii': re.compile(r'author/([A-Z0-9]+)', re.IGNORECASE)
+    # Extracts the alphanumeric ID following 'author/'
+    'cinii': re.compile(r'author/([A-Z0-9]+)', re.IGNORECASE),
+    # Extracts the ID string following 'page/' or 'resource/', ignoring trailing parameters
+    'dbpedia': re.compile(r'dbpedia\.org/(?:page|resource)/([^#?]+)', re.IGNORECASE),
+    # Extracts the numeric ID following 'subjectid='
+    'getty ulan': re.compile(r'subjectid=(\d+)', re.IGNORECASE),
+    # Extracts the ID string following 'nla.gov.au/', ignoring trailing parameters
+    'nla': re.compile(r'nla\.gov\.au/([^#?]+)', re.IGNORECASE),
+    # Extracts the numeric ID following 'request='
+    'nlaif': re.compile(r'request=(\d+)', re.IGNORECASE),
+    # Extracts the numeric ID following 'request='
+    'nli': re.compile(r'request=(\d+)', re.IGNORECASE),
+    # Extracts the numeric ID following 'request='
+    'nliaf': re.compile(r'request=(\d+)', re.IGNORECASE),
+    # Extracts the numeric ID following 'request='
+    'nlinsaf': re.compile(r'request=(\d+)', re.IGNORECASE),
+    # Extracts the ID string following 'aut/', ignoring trailing parameters
+    'nukat': re.compile(r'aut/([^#?]+)', re.IGNORECASE),
+    # Extracts the ID string following '10.1093/oi/', ignoring trailing parameters
+    'oxford reference': re.compile(r'10\.1093/oi/([^#?]+)', re.IGNORECASE),
+    # Extracts the numeric ID following 'num_dept='
+    'sycomore': re.compile(r'num_dept=(\d+)', re.IGNORECASE),
+    # Extracts the alphanumeric ID following 'udId='
+    'transcription du testament de pierre fournier par t. boudignon': re.compile(r'udId=([a-z0-9-]+)', re.IGNORECASE),
+    # Extracts the numeric ID following 'subjectid='
+    'ulan': re.compile(r'subjectid=(\d+)', re.IGNORECASE),
+
+    # New cases from unknown bases    
+    # Extracts the alphanumeric ID following 'ark:/12148/'
+    'catalogue bnf': re.compile(r'ark:/12148/([a-z0-9]+)', re.IGNORECASE),
+    # Extracts the alphanumeric ID following 'nome/'
+    'sbn': re.compile(r'nome/([A-Z0-9]+)', re.IGNORECASE),
+    # Extracts the numeric ID following 'descriptor-details/'
+    'dbn': re.compile(r'descriptor-details/(\d+)', re.IGNORECASE),
+    # Extracts the alphanumeric ID following 'nkp.cz/'
+    'nkp': re.compile(r'nkp\.cz/([a-z0-9]+)', re.IGNORECASE),
+    # Extracts the ID string following 'identities/', ignoring trailing parameters
+    'worldcat': re.compile(r'identities/([^#?]+)', re.IGNORECASE),
+    # Extracts the Q-number following 'wiki/'
+    'wikidata': re.compile(r'wiki/(Q\d+)', re.IGNORECASE)
 }
 
 def cleanExternalUid(baseName, currentUid, url):
